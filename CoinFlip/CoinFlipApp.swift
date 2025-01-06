1c5b8827fb77ff4fdc499561e4bae2cf4b0fea76
Vipin Nirmal <vipinnirmal251@gmail.com>
2025-01-06 09:08:05 +0530

    Initial Commit

---
+++ CoinFlip/CoinFlipApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlipApp.swift
+//  CoinFlip
+//
+//  Created by Vipin Nirmal on 06/01/25.
+//
+
+import SwiftUI
+
+@main
+struct CoinFlipApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoinFlipTests/CoinFlipTests.swift
@@ -0,0 +1,17 @@
+//
+//  CoinFlipTests.swift
+//  CoinFlipTests
+//
+//  Created by Vipin Nirmal on 06/01/25.
+//
+
+import Testing
+@testable import CoinFlip
+
+struct CoinFlipTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
